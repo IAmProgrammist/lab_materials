a09848bdc48039b64babdc302e99e9a4ebacf480
Vlad <vladislav.pakhomov74@gmail.com>
2025-01-18 21:10:25 +0300

    Merge with main

--- .quartz/quartz.config.ts
+++ .quartz/quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "ru-RU",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
